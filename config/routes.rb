aeb931baaa13722cdf19b671739d507bb80781b0
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2012-03-04 17:33:56 +0900

    create canvas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CsSandbox::Application.routes.draw do
+  get "top/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +51,7 @@ CsSandbox::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: "top#index"
 
   # See how all your routes lay out with "rake routes"
 
