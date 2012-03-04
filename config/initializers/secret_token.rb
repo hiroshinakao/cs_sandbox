cea1ef708b747df03d76ffbdc675f7decb058e43
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2012-03-04 17:26:25 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CsSandbox::Application.config.secret_token = '2cc113cabc4189a592fa3fc7120c367740be92bf42a6060a3e8d110816c31015eea4270c11e98e9cdc62d452fda40c312747d45d21c644c4ee5803bbb49aab54'
